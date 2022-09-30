9fb81aa356b45ba8e142bdf6770c15ac56d13877
Jun <junkumagai@gmail.com>
2022-10-01 03:27:44 +0900

    first commit

---
+++ streamlit_test.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.DataFrame({'Japanese': [55, 96, 76, 82,67],'Mathmatics': [44, 77, 54, 67, 88],'English': [67, 54, 76, 91, 68]})
+st.subheader('テスト結果')
+st.dataframe(df)
